806401ec5b8ca1e798017771698741e24e7c41a8
Alexandr Stepanov <stepanov.sh@gmail.com>
2013-08-12 18:53:45 +0300

    change login form

--- training/forms.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import forms
-from django.utils.translation import ugettext, ugettext_lazy as _
-
-class LoginForm(forms.AuthenticationForm):
-    username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'placeholder':'login'}))
-    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput(attrs={'placeholder':'login'}))
